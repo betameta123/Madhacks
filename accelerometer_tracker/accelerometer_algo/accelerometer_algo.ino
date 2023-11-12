d7f5d953ac0d77f45e4b580d8d26740bcf5370bf
betameta123 <kylehuang10@gmail.com>
2023-11-12 02:33:56 -0600

    Implement button with new algo

--- accelerometer_tracker/accelerometer_algo/accelerometer_algo.ino
+++ accelerometer_tracker/accelerometer_algo/accelerometer_algo.ino
@@ -6,6 +6,9 @@ int32_t sstatex = 0, sstatey = 0;
 int32_t a_x0 = 0, a_y0 = 0, v_x0 = 0, v_y0 = 0, pos_x0 = 0, pos_y0 = 0;
 uint8_t countx = 0, county = 0;
 
+const int buttonPin = 2;
+int buttonState = 0;
+
 void movement_end_check(int32_t a_x, int32_t a_y) {
   if(a_x == 0)
     countx++;
@@ -24,11 +27,8 @@ void movement_end_check(int32_t a_x, int32_t a_y) {
     v_y0 = 0;
   }
 }
-
-void setup() {
-  // put your setup code here, to run once:
+void accel_setup() {
   accelemeter.init();
-  Serial.begin(9600);
   int8_t ax, ay, az;
   uint16_t count1 = 0;
   do {
@@ -38,13 +38,18 @@ void setup() {
     count1++;
     Serial.println(count1);
   } while (count1 != 0x0400);
+
   sstatex >>= 10;
   sstatey >>= 10;
-  Serial.println("Setup Complete");
+}
+
+void setup() {
+  Serial.begin(9600);
+  accel_setup();
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-
   int8_t ax, ay, az;
   int32_t s_ax = 0, s_ay = 0, s_az = 0;
   uint8_t counter = 0;
@@ -61,23 +66,15 @@ void loop() {
   s_ax -= sstatex;
   s_ay -= sstatey;
 
-  // Serial.print(s_ax);
-  // Serial.print(",");
-  // Serial.println(s_ay);
-
-if (s_ax <= 3 && s_ax >= -3)
-  s_ax = 0;
-if (s_ay <= 3 && s_ay >= -3)
-  s_ay = 0;
+  if (s_ax <= 3 && s_ax >= -3)
+    s_ax = 0;
+  if (s_ay <= 3 && s_ay >= -3)
+    s_ay = 0;
 
   int32_t v_x = v_x0 + a_x0 + ((s_ax - a_x0) >> 1);
-  // Serial.print("Velocity x: ");
-  // Serial.print(v_x);
   int32_t pos_x = pos_x0 + v_x0 + ((v_x - v_x0) >> 1);
 
   int32_t v_y = v_y0 + a_y0 + ((s_ay - a_y0) >> 1);
-  // Serial.print("Velocity y: ");
-  // Serial.println(v_y);
   int32_t pos_y = pos_y0 + v_y0 + ((v_y - v_y0) >> 1);
 
   a_x0 = s_ax;
@@ -89,10 +86,14 @@ if (s_ay <= 3 && s_ay >= -3)
   Serial.print(pos_x);
   Serial.print(",");
   Serial.print(pos_y);
-  Serial.println("");
+  Serial.print(",");
 
   movement_end_check(s_ax, s_ay);
   pos_x0 = pos_x;
   pos_y0 = pos_y;
+
+  buttonState = digitalRead(buttonPin);
+  Serial.print(buttonState);
+  Serial.println("");
 }
 
